39601b2c7ef967d40c6da0ffa3d668580383c471
Jérémy Derussé <jeremy@derusse.com>
2025-12-04 21:57:52 +0100

    Disable Swagger UI and ReDoc when Swagger is off

--- src/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
+++ src/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
@@ -577,6 +577,9 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         }
 
         if (!$config['enable_swagger']) {
+            $container->setParameter('api_platform.enable_swagger_ui', false);
+            $container->setParameter('api_platform.enable_re_doc', false);
+
             return;
         }
 
